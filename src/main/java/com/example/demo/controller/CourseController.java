63415184b51ef25e236840f1f3c1ea33198b05b0
Yijun <wangyj2510@gmail.com>
2018-10-28 14:24:29 -0700

    firstVersion

---
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class CourseController {
+
+    @GetMapping(path = "/", produces = "application/json")
+    public HttpEntity findAllCourses(){
+       // List<CourseDto> allCourses = courseService.findAllCourses();
+
+        return new ResponseEntity<>("OK",HttpStatus.OK);
+    }
+}
